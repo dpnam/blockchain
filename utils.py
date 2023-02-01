ac90050f98c05a8384bacf4bee9d8c27b33f3902
dpnam <phuongnam.vl1997@gmail.com>
2023-02-01 23:30:23 +0700

    fix code encrypt and decrypt

--- utils.py
+++ utils.py
@@ -318,6 +318,16 @@ class EC(object):
         return R
     pass
 
+# func: convert decimal to hexadecimal with buff_size of hexadecimal
+def dec_to_hex(dec_number, buff_size):
+    dec_str = hex(dec_number)[2:]
+    for i in range(buff_size - len(dec_str)):
+        dec_str = '0' + dec_str
+    dec_str = '0x' + dec_str
+    
+    return dec_str
+
+# func: encrypt_ecc_unit
 def encrypt_ecc_unit(hex_str):
     # convert hex to dec
     x = int('0x' + hex_str, 0)
@@ -327,11 +337,11 @@ def encrypt_ecc_unit(hex_str):
     while (i <= 10000):
         fake_x = x*l + i
         try: 
-            M, mM = ec.at(fake_x)
+            M, mM = curr_ec.at(fake_x)
 
             condition_1 = (M.x == mM.x) and (M.x == fake_x)
-            condition_2 = (ec.neg(M) == mM)
-            condition_3 = ec.is_valid(M) and ec.is_valid(mM)
+            condition_2 = (curr_ec.neg(M) == mM)
+            condition_3 = curr_ec.is_valid(M) and curr_ec.is_valid(mM)
 
             if condition_1 and condition_2 and condition_3:
                 break
@@ -341,98 +351,158 @@ def encrypt_ecc_unit(hex_str):
         i += 1
             
     # encrypt
-    P = P1
-    Q_a = ec.mul(P, n_a)
-
-    M = M
+    Q_a = curr_ec.mul(P, n_a)
     k = 11
 
     # C_1 = k*P 
-    C_1 = ec.mul(P, k)
+    C_1 = curr_ec.mul(P, k)
 
     # C_2 = M + k*Q_a
-    C_2 = ec.add(M, ec.mul(Q_a, k))
+    C_2 = curr_ec.add(M, curr_ec.mul(Q_a, k))
     
     # sent encrypt
-    encrypt_x = dec_to_hex(C_1.x, buff_size_output)[2:] + dec_to_hex(C_2.x, buff_size_output)[2:]
-    encrypt_y = dec_to_hex(C_1.y, buff_size_output)[2:] + dec_to_hex(C_2.y, buff_size_output)[2:]
+    _C_1 = dec_to_hex(C_1.x, buff_size_output)[2:] + dec_to_hex(C_1.y, buff_size_output)[2:]
+    _C_2 = dec_to_hex(C_2.x, buff_size_output)[2:] + dec_to_hex(C_2.y, buff_size_output)[2:]
     
     # return
-    return encrypt_x, encrypt_y
+    return _C_1, _C_2
 
-def decrypt_ecc_unit(encrypt_x, encrypt_y):
+# func: decrypt_ecc_unit
+def decrypt_ecc_unit(_C_1, _C_2):
     # get C_1, C_2
-    C_1_x = int('0x' + encrypt_x[:buff_size_output], 0)
-    C_1_y = int('0x' + encrypt_y[:buff_size_output], 0)
+    C_1_x = int('0x' + _C_1[:buff_size_output], 0)
+    C_1_y = int('0x' + _C_1[buff_size_output:], 0)
     C_1 = Coord(x=C_1_x, y=C_1_y)
     
-    C_2_x = int('0x' + encrypt_x[buff_size_output: ], 0)
-    C_2_y = int('0x' + encrypt_y[buff_size_output: ], 0)
+    C_2_x = int('0x' + _C_2[:buff_size_output], 0)
+    C_2_y = int('0x' + _C_2[buff_size_output:], 0)
     C_2 = Coord(x=C_2_x, y=C_2_y)
     
     # C_2 – n_a*C_1
-    decrypt_point = ec.add(C_2, ec.neg(ec.mul(C_1, n_a)))
+    decrypt_point = curr_ec.add(C_2, curr_ec.neg(curr_ec.mul(C_1, n_a)))
     decrypt_x = int(decrypt_point.x//l)
     
     # return
     return hex(decrypt_x)[2:]
 
-# SAMPLE
-p = generate_prime(n=256)
+# func: encrypt_ecc_msg
+def encrypt_ecc_msg(msg):
+    # convert to hex_msg
+    hex_msg = msg.encode('utf-8').hex()
+    
+    # params
+    _C_1_s = ''
+    _C_2_s = ''
+
+    # loop encrypt with buff_size_input
+    curr = 0
+    while curr < len(hex_msg):
+        sub_hex_msg = hex_msg[curr:curr+buff_size_input]
+
+        _C_1, _C_2 = encrypt_ecc_unit(sub_hex_msg)
+
+        _C_1_s += _C_1
+        _C_2_s += _C_2
+
+        curr += buff_size_input
+
+    # return 
+    return _C_1_s, _C_2_s
+
+# func: decrypt_ecc_msg
+def decrypt_ecc_msg(_C_1_s, _C_2_s):
+    # params
+    dec_decrypt_s = ''
+    len_encrypt = len(_C_1_s) # or len(_C_2_s)
+
+    # loop decrypt with buff_size_output
+    curr = 0
+    while curr < len_encrypt:
+        _C_1 = _C_1_s[curr:curr+buff_size_output*2]
+        _C_2 = _C_2_s[curr:curr+buff_size_output*2]
+
+        dec_decrypt = str(decrypt_ecc_unit(_C_1, _C_2))
+        dec_decrypt_s += dec_decrypt
+
+        curr += buff_size_output*2
+
+    # convert string to hex
+    decrypt_msg = bytes.fromhex(hex(int('0x'+ dec_decrypt_s, 0))[2:]).decode('utf-8')
+    
+    # return
+    return decrypt_msg
+
+########################### SAMPLE ###########################
+# Key Exchange
+## init elliptic curve
+p = generate_prime(n=160)
+a = random.getrandbits(128) % p
+b = random.getrandbits(128) % p
+curr_ec = EC(a, b, p)
+buff_size_output = len((hex((1 << p.bit_length()) - 1))[2:])
+
+## random point P
+while True:
+    try:
+        x = random.getrandbits(128) % p
+        P = Coord(x=x, y=curr_ec.at(x)[0].y)
+        break
+        
+    except:
+        pass
+    
+## create Q_a and Q_b
+n_a = 123456789
+Q_a = curr_ec.mul(P, n_a)
+
+n_b = 987654321
+Q_b = curr_ec.mul(P, n_b)
+
+## share secret point
+assert curr_ec.mul(Q_a, n_b) == curr_ec.mul(Q_b, n_a)
+K = curr_ec.mul(Q_a, n_b)
+share_secret_key = '0x' + dec_to_hex(K.x, buff_size_output)[2:] + dec_to_hex(K.y, buff_size_output)[2:]
+
+print(K)
+print('Share Secret Key: ' + share_secret_key)
+
+# Encrypt/Decrypt
+## https://crypto.stackexchange.com/questions/76340/how-to-create-an-ec-point-from-a-plaintext-message-for-encryption
+p = generate_prime(n=160)
 a = random.getrandbits(128) % p
 b = random.getrandbits(128) % p
 
-ec = EC(a, b, p)
+curr_ec = EC(a, b, p)
 
-# params
-l = 35
+## params
+l = 41
 fake_p = p // l
 
 buff_size_input = len((hex((1 << fake_p.bit_length()) - 1))[2:]) - 1
 buff_size_output = len((hex((1 << p.bit_length()) - 1))[2:])
 
-# public key
-P_x = random.getrandbits(128)
-P = Coord(x=P_x % p, y=ec.at(P_x)[0].y)
+## public key
+while True:
+    try:
+        x = random.getrandbits(128) % p
+        P = Coord(x=x, y=curr_ec.at(x)[0].y)
+        break
+    except:
+        pass
 
-# private key
-n_a = 5
+## private key
+n_a = 124124
 
+## meassge
 msg = ''' 
 - Đặng Phương Nam 3131 trường đại học khoa học tự nhiên thành phố hồ chí minh 
 - abc adadwadawd
 '''
-hex_msg = msg.encode('utf-8').hex()
 
-# Encrypt
-encrypt_x_s = ''
-encrypt_y_s = ''
+## encrypt
+_C_1_s, _C_2_s = encrypt_ecc_msg(msg)
+decrypt_msg = decrypt_ecc_msg(_C_1_s, _C_2_s)
 
-curr = 0
-while curr < len(hex_msg):
-    sub_hex_msg = hex_msg[curr:curr+buff_size_input]
-    
-    encrypt_x, encrypt_y = encrypt_ecc_unit(sub_hex_msg)
-    
-    encrypt_x_s += encrypt_x
-    encrypt_y_s += encrypt_y
-    
-    curr += buff_size_input
-    
-# Decrypt
-dec_decrypt_s = ''
-len_encrypt = len(encrypt_x_s) # or len(encrypt_y_s)
-
-curr = 0
-while curr < len_encrypt:
-    encrypt_x = encrypt_x_s[curr:curr+buff_size_output*2]
-    encrypt_y = encrypt_y_s[curr:curr+buff_size_output*2]
-        
-    dec_decrypt = str(decrypt_ecc_unit(encrypt_x, encrypt_y))
-    dec_decrypt_s += dec_decrypt
-    
-    curr += buff_size_output*2
-    
-encrypt_str_x = encrypt_x_s
-encrypt_str_y = encrypt_y_s
-decrypt_str = bytes.fromhex(hex(int('0x'+ dec_decrypt_s, 0))[2:]).decode('utf-8')
+print(_C_1_s)
+print(_C_2_s)
+print(decrypt_msg)
